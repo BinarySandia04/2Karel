c4798edb00ed7086e29f45937dadeeb628d68000
Sandia! <aranseraroig@gmail.com>
2018-12-14 23:55:54 +0100

    First scene!

---
+++ 2KarelGame/Assets/BinSandia/Scripts/CodeRendering.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class CodeRendering : MonoBehaviour {
+
+	public void RenderTextColors(string text)
+    {
+        Debug.Log(text);
+        int startIndex = text.IndexOf("test");
+
+    }
+}
